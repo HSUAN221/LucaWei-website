69c3cb4c250bc36556f85dd8a4a7cc84c8093d44
LucaWei <lucawei@moldex3d.com>
2023-04-27 14:29:46 +0800

    add Heading in Page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
-import { Container, Box } from "@chakra-ui/react"
+import { Container, Box, Heading } from "@chakra-ui/react"
 
 const Page = () => {
     return (
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="grey" p={3} mb={6} align="center">
+                Hello, I'm Luca in Taiwnan!
+            </Box>
+
+            <Box display={{md:'flex'}}>
+                <Box floexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Luca Wei
+                    </Heading>
+                    <p>Software Engineer (Artist / Developer)</p>
+                </Box>
+            </Box>
+        </Container>
     )
 }
 
