ee051f397e5190b9ef1bb8bb645a9366598748e5
LucaWei <lucawei@moldex3d.com>
2023-05-02 23:33:39 +0800

    Work, Biography sections are completed

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,9 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
+import FootprintIcon from './icons/footprint'
 import styled from '@emotion/styled'
+
+import { SiRabbitmq } from "react-icons/si"
 
 const LogoBox = styled.span`
   font-weight: bold;
@@ -11,29 +13,32 @@ const LogoBox = styled.span`
   height: 30px;
   line-height: 20px;
   padding: 10px;
-  &:hover img {
+  > svg {
+    transition: 200ms ease;
+  }
+  &:hover > svg {
     transform: rotate(20deg);
   }
 `
 
 const Logo = () => {
-    const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+  return (
+    (<Link href="/" scroll={false}>
 
-    return (
-        <Link href="/">
-                <LogoBox>
-                    <Image src={footPrintImg} width={20} height={20} alt="logo" />
-                    <Text 
-                        color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                        fontFamily='M PLUS Rounded 1c'
-                        fontWeight="bold"
-                        ml={3}
-                    >
-                        Luca Wei
-                    </Text>
-                </LogoBox>
-        </Link>
-    )
+      <LogoBox>
+        <SiRabbitmq />
+        <Text
+          color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+          fontFamily='M PLUS Rounded 1c", sans-serif'
+          fontWeight="bold"
+          ml={3}
+        >
+          Luca Wei
+        </Text>
+      </LogoBox>
+
+    </Link>)
+  );
 }
 
 export default Logo
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,107 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import {
+    Container,
+    Box,
+    Heading,
+    Image,
+    useColorModeValue,
+    Link,
+    Button
+} from "@chakra-ui/react"
+import NextLink from "next/link"
+import { LinkIcon } from "@chakra-ui/icons"
+import Section from "../components/section"
+import Paragraph from "../components/paragraph"
+import { BioSection, BioYear } from "../components/bio"
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="grey" p={3} mb={6} align="center">
+            <Box
+            borderRadius="lg"
+            bg={useColorModeValue('whiteAlpha.700', 'whiteAlpha.100')}
+            p={3}
+            mb={6}
+            align="center"
+            css={{ backdropFilter: 'blur(10px)' }}
+            >
                 Hello, I'm Luca in Taiwnan!
             </Box>
 
+            {/* Profile */}
             <Box display={{md:'flex'}}>
                 <Box floexGrow={1}>
                     <Heading as="h2" variant="page-title">
                         Luca Wei
                     </Heading>
-                    <p>Software Engineer (Artist / Developer)</p>
+                    <p>Software Engineer (Artist / Developer / Designer )</p>
+                </Box>
+                <Box
+                flexShrink={0}
+                mt={{base: 4, md: 0}}
+                ml= {{md: 6}}
+                align="center"
+                >
+                    <Image
+                    borderColor="whiteAlpha.800"
+                    borderWidth={2}
+                    borderStyle="soild"
+                    maxWidth="100px"
+                    display="inline-block"
+                    borderRadius="full"
+                    src="/images/luca.jpg"
+                    alt="Profile Image"
+                    />
                 </Box>
             </Box>
+
+            {/* Work */}
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <Paragraph>
+                    Paragraph
+                </Paragraph>
+            </Section>
+            <Box align="center" my={4}>
+                <Button
+                as={NextLink}
+                href="/works"
+                scroll={false}
+                rightIcon={<LinkIcon />}
+                colorScheme="teal"
+                >
+                    My portfolio
+                </Button>
+            </Box>
+
+            {/* biography */}
+            <Section delay={0.2}>
+                <Heading as="h3" variant="section-title">
+                    Biography
+                </Heading>
+                <BioSection>
+                    <BioYear>1996</BioYear>
+                    Born in Taipei (台北), Taiwan.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2019</BioYear>
+                    Researched in Computational Fluid Dynamics Lab at Kobe University, Japan (神戶大學計算科學專攻)
+                </BioSection>
+                <BioSection>
+                    <BioYear>2020</BioYear>
+                    Completed the Master&apos;s Program in the Graduate School of
+                    Mechanical Engineering at National Taiwan University of Science and Technology
+                    (國立臺灣科技大學機械工程)
+                </BioSection>
+                <BioSection>
+                    <BioYear>2021 to present</BioYear>
+                    Worked at Moldex3D, Taiwan (科盛科技)
+                </BioSection>
+                
+            </Section>
         </Container>
     )
 }
 
-export default Page 
+export default Page
