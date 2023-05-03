bb40bc36192e989353dd3c8e1eabd712fe5b4e6b
LucaWei <lucawei@moldex3d.com>
2023-05-03 23:41:50 +0800

    fixed

--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,6 @@
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
-import Section from '../components/section'
+// import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading } from '@chakra-ui/react'
+// import Section from '../components/section'
 // import { WorkGridItem } from '../components/grid-item'
 
 // import thumbCfdsolver from '../public/images/works/cfd-solver.png'
@@ -10,14 +11,14 @@ const Works = () => {
             <Heading as="h3" fontSize={20} mb={4}>
                 Works
             </Heading>
-
+{/* 
             <SimpleGrid columns={[1, 1, 2]} gap={6}>
                 <Section>
-                    {/* <WorkGridItem id="cfdsolver" title="CFD Solver" thumbnail={thumbCfdsolver}>
+                    <WorkGridItem id="cfdsolver" title="CFD Solver" thumbnail={thumbCfdsolver}>
                     A 
-                    </WorkGridItem> */}
+                    </WorkGridItem>
                 </Section>
-            </SimpleGrid>
+            </SimpleGrid> */}
         </Container>
     )
 }
