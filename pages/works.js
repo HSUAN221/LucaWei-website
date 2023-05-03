ea7f4e02aa45f20fd4a165bae809627ffde13446
LucaWei <lucawei@moldex3d.com>
2023-05-03 23:21:53 +0800

    fix: 🐛 fix

--- pages/works.js
+++ pages/works.js
@@ -11,13 +11,13 @@ const Works = () => {
                 Works
             </Heading>
 
-            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+            {/* <SimpleGrid columns={[1, 1, 2]} gap={6}>
                 <Section>
-                    {/* <WorkGridItem id="cfdsolver" title="CFD Solver" thumbnail={thumbCfdsolver}>
+                    <WorkGridItem id="cfdsolver" title="CFD Solver" thumbnail={thumbCfdsolver}>
                     A 
-                    </WorkGridItem> */}
+                    </WorkGridItem>
                 </Section>
-            </SimpleGrid>
+            </SimpleGrid> */}
         </Container>
     )
 }
